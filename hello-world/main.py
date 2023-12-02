922f7b81b87ce30d0fb9da9a4c977ca6dbeaf58e
mrGoonies <gooniesdev@gmail.com>
2023-12-01 23:21:31 -0300

    Modified: Adding a space between the title and user input

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,5 +1,5 @@
 def get_user_name() -> str:
-    _input: str = input("What is your name? ")
+    _input: str = input("what is your name? ")
 
     return _input
 
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     print("-" * 25)
     print("\tHELLO APP")
     print("-" * 25)
+    print("\n")
 
     user_name: str = get_user_name()
-
     print(f"Nice to meet you {user_name}")
