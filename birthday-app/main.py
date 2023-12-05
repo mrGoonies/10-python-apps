45263ed29bde42f99fc629440803d8d17a7d09f9
mrGoonies <gooniesdev@gmail.com>
2023-12-04 23:07:09 -0300

    Add: Personalized messages and results between the year of birth an current year

--- birthday-app/main.py
+++ birthday-app/main.py
@@ -1,7 +1,26 @@
 from datetime import date
 
 
+def custom_message(difference_date: int) -> None:
+    """
+    We deliver a personalized message depending o the missing days of the user's birthday.
+
+    :param difference_date: Result betweeen the user's birthday date and the current day.
+    :return:
+        No value is returned, we only print a message upon fulfillment of a condition.
+    """
+    if difference_date == 0:
+        print("Happy Birthday!!")
+    elif difference_date < 0:
+        print(f"You had your birthday {difference_date} days ago this year.")
+    else:
+        print(f"These are the days left until your birthday!\n{difference_date}")
+
 def get_birthday_user() -> date:
+    """ Request the user's date of birth.
+
+    :return: The date if birth parsed from integer to date type.
+    """
     year_birthday: int = int(input("What year were you born [YYYY]?\n>> "))
     month_birthday: int = int(input("What month were you born [MM]?\n>> "))
     day_birthday: int = int(input("What day were you born [DD]?\n>> "))
@@ -11,17 +30,26 @@ def get_birthday_user() -> date:
 
     return date_birthday
 
+def compute_days_between_date(birthday_user: date) -> date.day:
+    """
+    We obtain the difference in days between the current year and the year of birth.
 
-def compute_days_between_date(birthday_user: date):
-    pass
+    :param birthday_user:
+    :return:
+    """
+    today_date: date = date.today()
+    date_difference = today_date - birthday_user
 
-
+    return date_difference.days
 if __name__ == "__main__":
-    todays_date: date = date.today()
+
     print("-" * 30)
     print("\tBirthday App")
     print("-" * 30)
     print()
-    
+
     birthday = get_birthday_user()
-    print(birthday)
+    result = compute_days_between_date(birthday)
+
+    custom_message(result)
+
