d0b396b4afdba7c6042a6d0679852f4c1a08a62b
mrGoonies <gooniesdev@gmail.com>
2023-12-10 16:47:45 -0300

    Modified: Refactoring absolute path of the journal.

--- daily-journal/journal.py
+++ daily-journal/journal.py
@@ -8,7 +8,13 @@ def load_journal() -> list:
 
 
 def save_journal(name: str, list_of_entries: List[str]) -> None:
-    filename = os.path.abspath(os.path.join("./journals", "{}.jrl".format(name)))
+    """
+    Save the journal in a file.
+    :param name: name of the journal.
+    :param list_of_entries: list of entries.
+    :return: None
+    """
+    filename = get_full_path(name)
     print(f"Your journal will be saved in {filename}.")
 
     with open(filename, "w") as file:
@@ -18,6 +24,17 @@ def save_journal(name: str, list_of_entries: List[str]) -> None:
     print("Saving...")
     time.sleep(1)
     print("Your journal has been saved.")
+
+
+def get_full_path(name: str) -> str:
+    """
+    Get the full path of the journal file.
+    :param name: name of the journal.
+    :return: full path of the journal file in a string.
+    """
+    filename = os.path.abspath(os.path.join("./journals", "{}.jrl".format(name)))
+
+    return filename
 
 
 def add_entry(entry: str, entries: List[str]) -> None:
